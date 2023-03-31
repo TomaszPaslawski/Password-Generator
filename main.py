53866a2a8441101fc2c814176d68cca5ff136773
TomaszPaslawski <paslawski.tom@gmail.com>
2023-03-31 13:17:07 +0200

    I added the user input validation to avoid incorrect values.

--- main.py
+++ main.py
@@ -10,9 +10,33 @@ from numbers import numbers as num
 from symbols import symbols as sym
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like to have in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like to have in your password?\n"))
-nr_numbers = int(input(f"How many numbers would you like to have in your password?\n"))
+
+while True:
+  try:
+    nr_letters = int(input("How many letters would you like to have in your password?\n"))
+  except ValueError:
+    print('You typed a non-numerical value. Please try again!')
+    continue
+  else:
+    break
+
+while True:
+  try:
+    nr_symbols = int(input(f"How many symbols would you like to have in your password?\n"))
+  except ValueError:
+    print('You typed a non-numerical value. Please try again!')
+    continue
+  else:
+    break
+
+while True:
+  try:
+    nr_numbers = int(input(f"How many numbers would you like to have in your password?\n"))
+  except ValueError:
+    print('You typed a non-numerical value. Please try again!')
+    continue
+  else:
+    break
 
 password_list = []
 
